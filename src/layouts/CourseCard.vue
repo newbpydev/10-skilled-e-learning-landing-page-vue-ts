6a6e4a518e62a99c36f0d9e1891d730a2bb752bc
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2022-10-28 18:58:13 -0300

    initial commit

---
+++ src/layouts/CourseCard.vue
@@ -0,0 +1,28 @@
+<!-- * SCRIPT -->
+<script setup lang="ts">
+// script goes here
+</script>
+
+<!-- * TEMPLATE -->
+<template>
+  <article class="course-card">
+    <div class="course-card__icon">
+      <slot name="icon" />
+    </div>
+
+    <h3 class="course-card__heading">
+      <slot name="heading" />
+    </h3>
+
+    <p class="course-card__text">
+      <slot name="text" class="course-card__text" />
+    </p>
+
+    <!-- TODO: add button here -->
+  </article>
+</template>
+
+<!-- * STYLE -->
+<style scoped>
+/* styles go here */
+</style>
